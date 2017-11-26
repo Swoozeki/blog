e2724f09c44de683167b0c3a91d9c5cc48452712
swoozeki <sujitkarki95@gmail.com>
2017-11-26 11:44:26 -0500

    added custom https redirection middleware

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ const MongoStore = require('connect-mongo')(expressSession);
 
 //custom middleware to redirect client from http to https if needed
 app.use((req, res, next) => {
-  console.log('This connection right now is: '+ req.headers['x-forwarded-proto']);
-  // if(req.app.get('env') === 'production' && req.protocol === 'http') 
-  //   return res.redirect('https://'+req.hostname+req.originalUrl);
+  if(req.app.get('env') === 'production' && req.headers['x-forwarded-proto'] === 'http') 
+    return res.redirect('https://'+req.hostname+req.originalUrl);
   next();
 });
-// const sslRedirect = require('heroku-ssl-redirect');
-// app.use(sslRedirect());
 
 //set up mongoose connection
 const mongoose = require('mongoose');
